42e9db43b805d5189135c332172989c6b10f2556
J Delaney <j.delaney.sd@gmail.com>
2016-01-28 13:12:41 -0800

    initial

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style>
+        body {
+            padding-top: 50px;
+            padding-bottom: 20px;
+        }
+    </style>
+    <!--<link rel="stylesheet" href="css/bootstrap-theme.min.css">-->
+    <link rel="stylesheet" href="css/main.css">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">SurfBoardBoard</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#"><span class="glyphicon glyphicon-list"></span> Your Listings</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> My Profile</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-envelope"></span> Messages <span style="color: darkred">(1)</span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-cog"></span> Account Settings</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+            </ul>
+        </div><!--/.navbar-collapse -->
+    </div>
+</nav>
+
+<div class="container" id="main">
+    <div style="margin: 0 auto; width: 100px; height: 100px;">
+        <img class="img-circle" style="width: 100px; height: 100px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0NClNvdXJjZSBVUkw6IGhvbGRlci5qcy8xNDB4MTQwDQpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNi4wLg0KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tDQooYykgMjAxMi0yMDE1IEl2YW4gTWFsb3BpbnNreSAtIGh0dHA6Ly9pbXNreS5jbw0KLS0+PGRlZnM+PHN0eWxlIHR5cGU9InRleHQvY3NzIj48IVtDREFUQVsjaG9sZGVyXzE1Mjg0MDNlNjVlIHRleHQgeyBmaWxsOiNBQUFBQUE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6MTBwdCB9IF1dPjwvc3R5bGU+PC9kZWZzPjxnIGlkPSJob2xkZXJfMTUyODQwM2U2NWUiPjxyZWN0IHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIiBmaWxsPSIjRUVFRUVFIi8+PGc+PHRleHQgeD0iNDQuMDU0Njg3NSIgeT0iNzQuNSI+TG9nbzwvdGV4dD48L2c+PC9nPjwvc3ZnPg==">
+    </div>
+
+    <button type="button" class="btn btn-primary btn-lg btn-block">Learn how it works</button>
+
+    <button type="button" class="btn btn-info btn-lg btn-block">Rent Out Your Gear <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
+
+    <!--<h2 class="text-center">or</h2>-->
+    <div style="height: 60px;"></div>
+
+    <button type="button" class="btn btn-info btn-lg btn-block">Rent From Others <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
+</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+<script src="js/vendor/bootstrap.min.js"></script>
+
+<script src="js/main.js"></script>
+</body>
+</html>
