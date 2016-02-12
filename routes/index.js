cd4eeed1a5043aab193e316b6e1b94a9aec8a969
Jennie Yoon <jey024@ucsd.edu>
2016-02-11 22:56:47 -0800

    Fixed link from list-message to link to list-steps page

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,10 @@ router.get('/list-gear/list-edit-description', function (request, response, next
     response.render('list-gear/list-edit-description');
 });
 
+router.get('/list-gear/list-edit-description', function (request, response, next) {
+    response.render('list-gear/list-steps-description');
+});
+
 router.get('/find-gear/choose-type', function (request, response, next) {
     response.render('find-gear/choose-type');
 });
