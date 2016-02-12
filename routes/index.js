d6341f3003d8331899c1e859465c7d059b94d46a
Jennie Yoon <jey024@ucsd.edu>
2016-02-11 19:38:48 -0800

    added message page before user can type in item detail

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,10 @@ router.get('/list-gear/list-choose-type', function (request, response, next) {
     response.render('list-gear/list-choose-type');
 });
 
+router.get('/list-gear/list-message', function (request, response, next) {
+    response.render('list-gear/list-message');
+});
+
 router.get('/find-gear/choose-type', function (request, response, next) {
     response.render('find-gear/choose-type');
 });
