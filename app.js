e5cab3c69c1bedbeff4a65b2e8d909c150c47f0b
J Delaney <j.delaney.sd@gmail.com>
2016-02-21 16:36:18 -0800

    Add blank settings

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var settings = require('./settings.json');
+
 var routes = require('./routes/index');
 
 var app = express();
