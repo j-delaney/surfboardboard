75fc83efbe4faf6cd65b4a3d0238008146e0a540
J Delaney <j.delaney.sd@gmail.com>
2016-02-28 12:33:46 -0800

    Remove annoying logging

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var hbs = require('hbs');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -24,7 +23,6 @@ app.set('view engine', 'hbs');
 require('./hbs-helpers');
 
 // app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
