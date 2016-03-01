eee142c0ae85236f67c029ca0ebd4bcd13d8e7aa
J Delaney <j.delaney.sd@gmail.com>
2016-03-01 10:46:57 -0800

    Track feedback

--- routes/api.js
+++ routes/api.js
@@ -3,26 +3,30 @@ var Track = require('./../models/track');
 
 module.exports = function (app) {
     app.post('/api/feedback', function (request, response, next) {
-        if (!request.isAuthenticated()) {
-            return response.status(401);
-        }
-
+        console.log('Doing feedback');
         var feedback = Feedback({
-            user: request.user.id,
             rating: request.body.rating || '',
             comment: request.body.comment || '',
             path: request.body.path || ''
         });
 
+        if (request.isAuthenticated()) {
+            console.log('User is logged in for feedback');
+            feedback.user = request.user.id
+        }
+
         if (!util.isNullOrUndefined(request.session.track)) {
+            console.log('Setting session for feedback');
             feedback.session = request.session.track;
         }
 
         feedback.save(function (err, feedback) {
             if (err) {
+                console.log('Error saving feedback');
                 throw err;
             }
 
+            console.log('Returning success for feedback');
             return response.status(200);
         });
     });
