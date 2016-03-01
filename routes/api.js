7311603b5b562905c8295122aee4ed3a54fd8838
J Delaney <j.delaney.sd@gmail.com>
2016-03-01 10:58:46 -0800

    Fix feedback

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 var Feedback = require('./../models/feedback');
 var Track = require('./../models/track');
+var util = require('util');
 
 module.exports = function (app) {
     app.post('/api/feedback', function (request, response, next) {
-        console.log('Doing feedback');
         var feedback = Feedback({
             rating: request.body.rating || '',
             comment: request.body.comment || '',
@@ -11,22 +11,18 @@ module.exports = function (app) {
         });
 
         if (request.isAuthenticated()) {
-            console.log('User is logged in for feedback');
-            feedback.user = request.user.id
+            feedback.user = request.user.id;
         }
 
         if (!util.isNullOrUndefined(request.session.track)) {
-            console.log('Setting session for feedback');
             feedback.session = request.session.track;
         }
 
         feedback.save(function (err, feedback) {
             if (err) {
-                console.log('Error saving feedback');
                 throw err;
             }
 
-            console.log('Returning success for feedback');
             return response.status(200);
         });
     });
